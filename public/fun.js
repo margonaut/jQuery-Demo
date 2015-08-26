2130acf4335972f44fe28407adf77b6ec25c36a3
margonaut <margaret.morgane@gmail.com>
2015-08-26 19:04:34 -0400

    First event listener working

---
+++ public/fun.js
@@ -0,0 +1,22 @@
+
+
+// On Click, alert
+
+var jsAlert = document.getElementById("alert");
+
+jsAlert.addEventListener("click", function() {
+  alert("wooo!");
+})
+
+
+// Hover Change Color
+
+var jsChangeColor = document.getElementById("changecolor");
+
+jsChangeColor.addEventListener("hover", function() {
+  alert("woo!")
+})
+
+// Hover Grow
+
+var jsGrow = document.getElementById("grow");
